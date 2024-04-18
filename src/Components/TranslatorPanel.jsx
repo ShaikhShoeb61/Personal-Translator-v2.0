3ce660f85fbd3976fa8f225cc6190face12f8583
ShaikhShoeb61 <shoaibshaikh.ss639@gmail.com>
2024-04-19 01:45:37 +0530

    bug: fixes bugs and refactor code

--- src/Components/TranslatorPanel.jsx
+++ src/Components/TranslatorPanel.jsx
@@ -1,4 +1,6 @@
-export const translatorPanel = ({ HeaderSection, FooterSection, TextField }) => {
+import React from "react";
+
+const translatorPanel = ({ HeaderSection, TextField, FooterSection }) => {
   return (
     <div className="w-[20.5rem] h-[28rem] rounded-2xl border border-customgray-300 border-opacity-50 flex flex-col justify-between">
       {HeaderSection}
@@ -8,3 +10,4 @@ export const translatorPanel = ({ HeaderSection, FooterSection, TextField }) => 
   );
 };
 
+export default translatorPanel;
